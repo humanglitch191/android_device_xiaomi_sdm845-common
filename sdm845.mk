363f7b8230d434fa1ebdda1d79f75fea3191d022
Henrique Pereira <hlcpereira@pixelexperience.org>
2020-05-01 14:48:59 -0300

    sdm845-common: powerstats: Pixel -> Xiaomi
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>
    Change-Id: Ic4d3704ba950c21aa186b1aebdedbcaab0cb6085

--- sdm845.mk
+++ sdm845.mk
@@ -252,7 +252,7 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr \
-    android.hardware.power.stats@1.0-service.pixel
+    android.hardware.power.stats@1.0-service.xiaomi
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
