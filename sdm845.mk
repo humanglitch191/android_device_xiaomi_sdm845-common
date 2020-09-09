930d42b4044f8308d43907c0992c7b633b17757f
Signed-off-by: Erfan Abdi <erfangplus@gmail.com>
2020-09-09 19:45:10 +0200

    sdm845-common: Define QCOM Hardware
    Change-Id: Iddbc6ae3e21cebab3f55b4907abc08527523b81a

--- sdm845.mk
+++ sdm845.mk
@@ -19,6 +19,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
     $(LOCAL_PATH)/configs/component-overrides_qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/component-overrides.xml
 
+# Board
+PRODUCT_USES_QCOM_HARDWARE := true
+PRODUCT_BOARD_PLATFORM := sdm845
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
