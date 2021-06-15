4a8d96389787de0dad6ad2cb2628adfc54956cdf
Vaisakh Murali <mvaisakh@statixos.com>
2021-06-15 19:18:35 +0200

    sdm845-common: Use EvaGCC to compile kernel
    Change-Id: Ia884c3c2c8ca1abfeb517fe8b91e0d9c7fe09f22

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -36,11 +36,11 @@ BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA840
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
-TARGET_KERNEL_ARCH := arm64
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-  TARGET_KERNEL_CLANG_COMPILE := true
-  TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm845
-endif
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-elf-
+KERNEL_TOOLCHAIN := $(PWD)/prebuilts/gcc/linux-x86/aarch64/aarch64-elf/bin
+TARGET_GCC_ARM32_TOOLCHAIN := $(PWD)/prebuilts/gcc/linux-x86/arm/arm-eabi/bin/arm-eabi-
+KERNEL_LD := CROSS_COMPILE_ARM32=$(TARGET_GCC_ARM32_TOOLCHAIN)
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm845
 
 # Platform
 TARGET_BOARD_PLATFORM := sdm845
